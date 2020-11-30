d25c58119d1a562d7795fde97bd32c575d84bcb0
Kenny <nobodykenny@gmail.com>
2020-11-30 17:45:50 +0300

    'work'

---
+++ src/blocks/modules/scroll-block/scroll-block.js
@@ -0,0 +1,13 @@
+modules.define('scroll-block', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
